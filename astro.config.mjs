f3b4f38daac1b1b1790a58af857113706b2728e7
Fastman <gabayt0_0@icloud.com>
2024-04-19 23:20:22 +0900

    feat: @astro/rss の導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import { config } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
+	site: config.website,
 	integrations: [tailwind()],
 });
--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export const author = "fastman";
 export const config = {
 	title: `${author} のブログ`,
 	description: `${author} のブログです`,
-	website: "blog.fastman.dev",
+	website: "https://blog.fastman.dev",
 };
