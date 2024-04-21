a229c1086dd1b24d28fce5a1f87013cb3e6f7c49
Fastman <gabayt0_0@icloud.com>
2024-04-21 14:38:55 +0900

    feat: sitemap を導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import { config } from "./src/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	site: config.website,
-	integrations: [tailwind()],
+  site: config.website,
+  integrations: [tailwind(), sitemap()]
 });
